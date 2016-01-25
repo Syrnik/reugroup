d6c5296b1c94600ba6591d35fbe32a08b9e5ec20
Serge Rodovnichenko <serge@syrnik.com>
2016-01-26 00:26:06 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Assign group on signup',
+  'icon' => 'img/reugroup.gif',
+  'version' => '1.0.0',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/shopReugroupPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class shopReugroupPlugin extends shopPlugin
+{
+
+}
