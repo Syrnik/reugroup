ea02389c96c7aa7ecdda1197d306faac456128cf
Serge Rodovnichenko <sergerod@gmail.com>
2023-07-09 17:42:33 +0300

    Добавлены необходимые мета-файлы

---
+++ .phpstorm.meta.php
@@ -0,0 +1,5 @@
+<?php
+
+namespace PHPSTORM_META {
+    override(\waSystem::getPlugin(), map(['reugroup' => shopReugroupPlugin::class]));
+}
---
+++ lib/config/requirements.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'php' => ['version' => '>=7.2.0', 'strict' => true],
+];
